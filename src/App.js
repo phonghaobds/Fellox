6e9d6116523ab96b6c67fc2250e346ba4ef02a03
phong47047 <phong47047@donga.edu.vn>
2023-03-30 16:25:55 +0700

    clean React architecture

--- src/App.js
+++ src/App.js
@@ -1,114 +1,16 @@
+import React from "react";
 import "./App.scss";
+//custom components
+import AppBar from "components/AppBar/AppBar";
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
 
 function App() {
   return (
     <div className="fellox-master">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.squarespace-cdn.com/content/v1/5d9fa3d14b2d110c1d455300/1603724603131-AM9TJCBWE0RBW0JP7TXN/scene+3.jpg?format=1000w"
-                alt="Img"
-              />
-              Title: PhongLe
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.squarespace-cdn.com/content/v1/5d9fa3d14b2d110c1d455300/1603724603131-AM9TJCBWE0RBW0JP7TXN/scene+3.jpg?format=1000w"
-                alt="Img"
-              />
-              Title: PhongLe
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.squarespace-cdn.com/content/v1/5d9fa3d14b2d110c1d455300/1603724603131-AM9TJCBWE0RBW0JP7TXN/scene+3.jpg?format=1000w"
-                alt="Img"
-              />
-              Title: PhongLe
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.squarespace-cdn.com/content/v1/5d9fa3d14b2d110c1d455300/1603724603131-AM9TJCBWE0RBW0JP7TXN/scene+3.jpg?format=1000w"
-                alt="Img"
-              />
-              Title: PhongLe
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.squarespace-cdn.com/content/v1/5d9fa3d14b2d110c1d455300/1603724603131-AM9TJCBWE0RBW0JP7TXN/scene+3.jpg?format=1000w"
-                alt="Img"
-              />
-              Title: PhongLe
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstore</header>
-          <ul>
-            <li>
-              <img
-                src="https://images.squarespace-cdn.com/content/v1/5d9fa3d14b2d110c1d455300/1603724603131-AM9TJCBWE0RBW0JP7TXN/scene+3.jpg?format=1000w"
-                alt="Img"
-              />
-              Title: PhongLe
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
