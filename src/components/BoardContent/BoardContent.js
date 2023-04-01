0d5ef394fef9a1d14e0e9dd23b203019bc427d9b
phong47047 <phong47047@donga.edu.vn>
2023-04-01 20:19:29 +0700

    implement drag and drop

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Container, Draggable } from "react-smooth-dnd";
 import { isEmpty } from "lodash";
 import "./BoardContent.scss";
 import Column from "components/Column/Column";
@@ -27,11 +28,28 @@ function BoardContent() {
       </div>
     );
   }
+  const onColumnDrop = (dropResult) => {
+    console.log(dropResult);
+  };
   return (
     <div className="board-content">
-      {columns.map((column, index) => (
-        <Column key={index} column={column} />
-      ))}
+      <Container
+        orientation="horizontal"
+        onDrop={onColumnDrop}
+        getChildPayload={(index) => columns[index]}
+        dragHandleSelector=".column-drag-handle"
+        dropPlaceholder={{
+          animationDuration: 150,
+          showOnTop: true,
+          className: "column-drop-preview",
+        }}
+      >
+        {columns.map((column, index) => (
+          <Draggable key={index}>
+            <Column column={column} />
+          </Draggable>
+        ))}
+      </Container>
     </div>
   );
 }
